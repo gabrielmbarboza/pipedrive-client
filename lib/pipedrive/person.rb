58d205f0064b4efb0256efb807c25dc7b4ef59df
Gabriel Moraes <gmoraes@ie.com.br>
2017-07-21 13:27:35 -0300

    Fixed duplicate person error on find_by_email method

--- lib/pipedrive/person.rb
+++ lib/pipedrive/person.rb
@@ -45,17 +45,15 @@ module Pipedrive
           raise "Email address invalid."
         end
 
-        user_email = email.split("@").first
-
-        res = get "/persons/find", query: {term: user_email}
+        res = get "/persons/find", query: {term: email, search_by_email: 1}
         
-        data = res["data"]
+        # data = res["data"]
 
-        if (data && data.size > 1 )
-          res["data"] = data.collect { |person|
-            if person["email"] == email then person end
-          }
-        end
+        # if (data && data.size > 1 )
+        #   res["data"] = data.collect { |person|
+        #     if person["email"] == email then person end
+        #   }
+        # end
         
         new(res.parsed_response)
       end
